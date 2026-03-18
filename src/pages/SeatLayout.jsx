e7c35154fa6041dfa66d77f7f122609090d1fdae
malleeswari-00 <malleeswarimukalla@gmail.com>
2026-03-18 19:58:31 +0530

    Updated MovieDetails,SeatLayou and created Loading

--- src/pages/SeatLayout.jsx
+++ src/pages/SeatLayout.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { dummyDateTimeData, dummyShowsData } from '../../assets/assets'
+import Loading from '../components/Loading'
+import { ClockIcon } from 'lucide-react'
 
 const SeatLayout = () => {
-  return (
-    <div>SeatLayout</div>
+  const {id,date}=useParams()
+  const [selectedSeats,setSelectedSeats]=useState([])
+  const [selectedTime,setSelectedTime]=useState(null)
+  const [show,setShow]=useState(null)
+
+  const navigate=useNavigate()
+  
+  const getShow=async()=>{
+    const show=dummyShowsData.find(show=>show._id===id)
+    if(show){
+      setShow({
+        movie:show,
+        dateTime:dummyDateTimeData
+      })
+    }
+  }
+
+useEffect(()=>{
+  getShow()
+
+},[])
+  return show?(
+    <div className='flex flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30
+    md:pt-50'>
+      {/* Available Timings */}
+      <div className='w-60 bg-primary/10 border border-primary/20 rounded-lg py-10
+      h-max md:sticky md:top-30'>
+        <p className='text-lg font-semibold px-6'>Available Timings</p>
+        <div>
+          {show.dateTime[date].map((item)=>(
+            <div>
+              <ClockIcon/>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* Seats Layout */}
+      <div></div>
+    </div>
+  ):(
+    <Loading/>
   )
 }
 
