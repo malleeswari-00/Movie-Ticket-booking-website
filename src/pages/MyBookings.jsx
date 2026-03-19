5f30985bccb531df6ca6836ae1e94e277d5ce7d0
malleeswari-00 <malleeswarimukalla@gmail.com>
2026-03-19 20:27:43 +0530

    updated MyBookings,SeatLayout and Created isoTimeFormat

--- src/pages/MyBookings.jsx
+++ src/pages/MyBookings.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { dummyBookingData } from '../../assets/assets'
+import Loading from '../components/Loading'
+import BlurCircle from '../components/BlurCircle'
 
 const MyBookings = () => {
-  return (
-    <div>MyBookings</div>
+  const currency=import.meta.env.VITE_CURRENCY
+  
+  const [bookings,setBookings]=useState([])
+  const [isLoading,setIsLoading]=useState(true)
+
+  const getMyBookings=async ()=>{
+    setBookings(dummyBookingData)
+    setIsLoading(false)
+  }
+
+  useEffect(()=>{
+    getMyBookings()
+
+  },[])
+
+  return isLoading ?(
+    <div className='relative px-6 md:px-16 lg:px-40 pt-30 md:pt-40 min-h-[80hv]'>
+      <BlurCircle top="100px" left="100px"/>
+      <div>
+        <BlurCircle bottom="0px"  left="600px" />
+      </div>
+      <h1 className='text-lg font-semibold mb-4'></h1>
+    </div>
+  ):(
+    <Loading/>
   )
 }
 
